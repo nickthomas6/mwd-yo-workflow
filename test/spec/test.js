3e7acd5ef4c2062cb3d3b272ff4220bb90a771cc
Nick Thomas <nthomas@idahopower.com>
2016-03-07 15:10:40 -0700

    initial commit

---
+++ test/spec/test.js
@@ -0,0 +1,17 @@
+(function () {
+  'use strict';
+
+  describe('Give it some context', function () {
+    describe('maybe a bit more context here', function () {
+      it('should run here few assertions', function () {
+
+      });
+      it('TEST 2', function(){
+
+      });
+      it('TEST 3 - Nick is the best!', function(){
+
+      });
+    });
+  });
+})();
