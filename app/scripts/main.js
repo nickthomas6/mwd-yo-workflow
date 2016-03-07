4ea20077c255ce9755eb1d5e7c0958b741d9cde1
Nick Thomas <nthomas@idahopower.com>
2016-03-07 15:28:53 -0700

    update gh-pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,4 @@
-console.log('\'Allo \'Allo!');
+/*global $:false, moment:false*/
+
+const datetime = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
+$('#datetime').text(datetime);
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -166,3 +166,8 @@ gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
 gulp.task('default', ['clean'], () => {
   gulp.start('build');
 });
+
+gulp.task('deploy', ['build'], function() {
+  return gulp.src('./dist/**/*')
+    .pipe($.ghPages());
+});
